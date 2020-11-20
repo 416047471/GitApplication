0991642d719b6935e1c2187f57a9c7553a408385
THR <416047471@qq.com>
2020-11-20 14:59:10 +0800

    加入创建数据库和表的各项配置

---
+++ app/src/main/java/com/example/centerweather/db/County.java
@@ -0,0 +1,40 @@
+package com.example.centerweather.db;
+
+public class County {
+    private int id;
+    private String countyName;//县的名字
+    private String weatherId;//县对应的天气id
+    private int cityID;//当前县所属市的ID值
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCountyName() {
+        return countyName;
+    }
+
+    public void setCountyName(String countyName) {
+        this.countyName = countyName;
+    }
+
+    public String getWeatherId() {
+        return weatherId;
+    }
+
+    public void setWeatherId(String weatherId) {
+        this.weatherId = weatherId;
+    }
+
+    public int getCityID() {
+        return cityID;
+    }
+
+    public void setCityID(int cityID) {
+        this.cityID = cityID;
+    }
+}
